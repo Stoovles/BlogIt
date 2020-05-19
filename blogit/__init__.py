0d9a0cb3eb4c8f0e68901b3493b0f83ce15e968a
Ethan Grab <ethangrab@gmail.com>
2020-05-19 12:44:46 -0600

    Changed database config

--- blogit/__init__.py
+++ blogit/__init__.py
@@ -7,7 +7,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db?check_same_thread=False'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///development.db?check_same_thread=False'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
