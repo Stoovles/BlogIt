36941a571621677f25ce805e747cc797cc815d2a
Ethan Grab <ethangrab@gmail.com>
2020-05-21 16:41:35 -0600

    Created app factory for different environments - test, dev, production

---
+++ tests/test_blogit.py
@@ -0,0 +1,18 @@
+
+def login(client, username, password):
+    return client.post('/login', data=dict(
+        username=username,
+        password=password
+    ), follow_redirects=True)
+
+
+def logout(client):
+    return client.get('/logout', follow_redirects=True)
+
+
+def test_empty_db(client):
+    """Start with a blank database."""
+
+    rv = client.get('/')
+    assert b'No entries here so far' in rv.data
+
