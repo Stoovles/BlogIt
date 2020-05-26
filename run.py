3904703d7cd73ad18db657ad11139a8221b32b61
Ethan Grab <ethangrab@gmail.com>
2020-05-26 11:34:34 -0600

    Comment

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-from blogit import create_development_app
+from blogit import create_development_app, create_test_app
 
 if __name__ == '__main__':
     create_development_app().run(debug=True)
+    # create_test_app().run(debug=True)
